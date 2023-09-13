dcbfe238dde8dbe48e4bcd8e3dfd1ee47e04dcee
Septillion24 <noahgrosh@gmail.com>
2023-09-13 12:19:49 -0400

    working matrix multiplication

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Matrix matrix1 = new Matrix(new List<List<float>>() 
+{
+    new List<float> {1, 2},
+    new List<float> {2, 3}
+}
+);
+
+Matrix matrix2 = new Matrix(new List<List<float>>() 
+{
+    new List<float> {3, 4},
+    new List<float> {1, 5}
+}
+);
+
+
+Console.WriteLine(matrix1.multiply(matrix2).ToString());
